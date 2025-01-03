c3c6af3b3c323af4177c57ca379d993557cf05e5
mcdmaisd <tkim0156@gmail.com>
2025-01-03 19:41:08 +0900

    Initial Commit

---
+++ Travel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Travel
+//
+//  Created by ilim on 2025-01-03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
